806d2e17fca6a12cc09d070f1daa788f8ae717a2
Michael Wirth <michael.wirth@mimacom.com>
2017-12-19 21:05:21 +0100

    JR2-596 Trigger deployment by URL

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 echo 'publish to Artifactory'
 ./gradlew bootJar publish -Pprod -Pgraphite -Pprometheus -x test --stacktrace
-echo 'prepare SSH connection'
-chmod 600 .travis/ssh_key
-ssh-keygen -p -P $SSH_PASS -N '' -f .travis/ssh_key
 echo 'deployment to DEV'
-ssh -i .travis/ssh_key ubuntu@159.100.254.166 "~/bin/deploy-jobroom.sh $TRAVIS_REPO_SLUG $TRAVIS_JOB_ID $TRAVIS_JOB_NUMBER"
+curl "https://dev.job-room.ch:8443/deploy/jr2?$TRAVIS_REPO_SLUG&$TRAVIS_JOB_ID&$TRAVIS_JOB_NUMBER" --insecure
 echo 'deployment was successful'
-
