cd709909235a14ca1f9eb54b1ff88a98c15f70c9
Michael Wirth <michael.wirth@mimacom.com>
2018-03-28 17:35:18 +0200

    Remove deploy.sh script call

--- .travis/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo 'publish to Artifactory'
-./gradlew bootJar publish -Pprod -Pzipkin -x test --stacktrace
-echo 'deployment to DEV'
-curl "https://dev.job-room.ch:8443/deploy/jr2?$TRAVIS_REPO_SLUG&$TRAVIS_JOB_ID&$TRAVIS_JOB_NUMBER" --insecure
-echo 'deployment was successful'
