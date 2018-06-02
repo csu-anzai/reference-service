b333151fb3abe264b35c30bf822aace8473a3ffa
Michael Wirth <michael.wirth@mimacom.com>
2018-06-02 14:47:09 +0200

    Evict all caches when reindexing

--- src/main/java/ch/admin/seco/service/reference/service/impl/ElasticsearchIndexServiceImpl.java
+++ src/main/java/ch/admin/seco/service/reference/service/impl/ElasticsearchIndexServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
 import ch.admin.seco.service.reference.service.ElasticsearchIndexService;
@@ -15,21 +17,31 @@ public class ElasticsearchIndexServiceImpl implements ElasticsearchIndexService 
     private final Logger log = LoggerFactory.getLogger(ElasticsearchIndexServiceImpl.class);
     private final ElasticsearchLocalityIndexer elasticsearchLocalityIndexer;
     private final ElasticsearchOccupationLabelIndexer elasticsearchOccupationLabelIndexer;
+    private final CacheManager cacheManager;
 
     ElasticsearchIndexServiceImpl(ElasticsearchLocalityIndexer elasticsearchLocalityIndexer,
-        ElasticsearchOccupationLabelIndexer elasticsearchOccupationLabelIndexer) {
+            ElasticsearchOccupationLabelIndexer elasticsearchOccupationLabelIndexer, CacheManager cacheManager) {
 
         this.elasticsearchLocalityIndexer = elasticsearchLocalityIndexer;
         this.elasticsearchOccupationLabelIndexer = elasticsearchOccupationLabelIndexer;
+        this.cacheManager = cacheManager;
     }
 
     public void reindexAll() {
 
         CompletableFuture.allOf(
-            CompletableFuture.runAsync(elasticsearchLocalityIndexer::reindexLocalities),
-            CompletableFuture.runAsync(elasticsearchOccupationLabelIndexer::reindexOccupationLabel)
+                CompletableFuture.runAsync(elasticsearchLocalityIndexer::reindexLocalities),
+                CompletableFuture.runAsync(elasticsearchOccupationLabelIndexer::reindexOccupationLabel)
         ).join();
 
+        evictAllCaches();
+
         log.info("ReindexAll finished");
+    }
+
+    private void evictAllCaches() {
+        cacheManager.getCacheNames().stream()
+                .map(cacheManager::getCache)
+                .forEach(Cache::clear);
     }
 }
